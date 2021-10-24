419c62f16ee7f49ee845a2cdf9bfac497dad49b8
atmatto <mattoover@gmail.com>
2021-10-24 13:59:34 +0200

    Create a better test.sh script

--- test.sh
+++ test.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-(rm -r ~/.local/share/Anki2/addons21/DuolingoDictionary/
-cp -r . ~/.local/share/Anki2/addons21/DuolingoDictionary/) &&
+rm -r ~/.local/share/Anki2/addons21/DuolingoDictionary/
+mkdir ~/.local/share/Anki2/addons21/DuolingoDictionary/
+cp -r __init__.py icon config.md manifest.json ~/.local/share/Anki2/addons21/DuolingoDictionary/
+# config-test.json will be used instead of config.json if it exists
+[ -f config-test.json ] && cp config-test.json ~/.local/share/Anki2/addons21/DuolingoDictionary/config.json || cp config.json ~/.local/share/Anki2/addons21/DuolingoDictionary/config.json
 anki
