60993e4d895cb66678b64477cd2edbe4b6609b42
atmatto <mattoover@gmail.com>
2022-03-27 16:03:39 +0200

    Make http requests asynchronous

--- __init__.py
+++ __init__.py
@@ -4,7 +4,10 @@ from anki.notes import Note
 from anki.models import NoteType
 from typing import List
 from pathlib import Path
-import requests
+from uuid import uuid4
+import json
+
+nam = qt.QNetworkAccessManager()
 
 def icon() -> str:
 	assert mw is not None
@@ -54,39 +57,43 @@ class Word:
 				label.setText(text)
 			return func
 
+		def loaded(dictionary_results):
+			loading.setVisible(False)
+			loading.setText("Loaded")
+			used_examples = set()
+			for example in dictionary_results:
+				if example["text"] not in used_examples:
+					used_examples.add(example["text"])
+					example_layout = qt.QHBoxLayout()
+					example_layout.setContentsMargins(0,0,0,0)
+					example_label = qt.QLabel()
+					if config["hide_example_translations"]:
+						example_label.setText("<div style=\"font-size: 16pt\">" + conv_html(example["example_sentence"]) + "</div>")
+					else:
+						example_label.setText("<div style=\"font-size: 16pt\">" + conv_html(example["example_sentence"]) + "</div>" + "<div>" + conv_html(example["translation"]) + "</div>")
+					example_button = qt.QPushButton()
+					qt.qconnect(example_button.clicked, paste_example(config["example_paste_format"].format(tl=example["text"], nl=example["translation_text"])))
+					example_button.setText("")
+					example_button.setFixedWidth(20)
+					example_button.setFixedHeight(20)
+					example_widget = qt.QWidget()
+					example_widget.setLayout(example_layout)
+					example_layout.addWidget(example_button, 0)
+					example_layout.addWidget(example_label, 1)
+					if config["hide_example_translations"]:
+						show_translation = qt.QPushButton()
+						show_translation.setText("Show translation")
+						qt.qconnect(show_translation.clicked, show_translation_f(example_label, "<div style=\"font-size: 16pt\">" + conv_html(example["example_sentence"]) + "</div>" + "<div>" + conv_html(example["translation"]) + "</div>"))
+						example_layout.addWidget(show_translation, 0)
+					examples_layout.addWidget(example_widget)
+		
+
 		def clicked():
 			if loading.text() != "Loaded":
 				loading.setText("Loading examples...")
-				dictionary_results = dictionary(search_result["lexemeId"])
-				loading.setVisible(False)
-				loading.setText("Loaded")
-				used_examples = set()
-				for example in dictionary_results:
-					if example["text"] not in used_examples:
-						used_examples.add(example["text"])
-						example_layout = qt.QHBoxLayout()
-						example_layout.setContentsMargins(0,0,0,0)
-						example_label = qt.QLabel()
-						if config["hide_example_translations"]:
-							example_label.setText("<div style=\"font-size: 16pt\">" + conv_html(example["example_sentence"]) + "</div>")
-						else:
-							example_label.setText("<div style=\"font-size: 16pt\">" + conv_html(example["example_sentence"]) + "</div>" + "<div>" + conv_html(example["translation"]) + "</div>")
-						example_button = qt.QPushButton()
-						qt.qconnect(example_button.clicked, paste_example(config["example_paste_format"].format(tl=example["text"], nl=example["translation_text"])))
-						example_button.setText("")
-						example_button.setFixedWidth(20)
-						example_button.setFixedHeight(20)
-						example_widget = qt.QWidget()
-						example_widget.setLayout(example_layout)
-						example_layout.addWidget(example_button, 0)
-						example_layout.addWidget(example_label, 1)
-						if config["hide_example_translations"]:
-							show_translation = qt.QPushButton()
-							show_translation.setText("Show translation")
-							qt.qconnect(show_translation.clicked, show_translation_f(example_label, "<div style=\"font-size: 16pt\">" + conv_html(example["example_sentence"]) + "</div>" + "<div>" + conv_html(example["translation"]) + "</div>"))
-							example_layout.addWidget(show_translation, 0)
-						examples_layout.addWidget(example_widget)
-		
+				self.dictionary_request(search_result["lexemeId"], loaded)
+				dictionary_results = list()
+
 		# Tab
 
 		w = qt.QWidget()
@@ -100,9 +107,37 @@ class Word:
 	def current_changed(self, i: int):
 		self.shown_funcs[i]()
 
+	def cleanup(self):
+		nam.finished.disconnect(self.on_response)
+
+	def on_response(self, r):
+		req_id = r.request().attribute(qt.QNetworkRequest.User)
+		w = r.request().attribute(qt.QNetworkRequest.UserMax)
+		if req_id in self.waiting_for:
+			results = list()
+			try:
+				results = json.loads(str(r.readAll(), "utf-8"))["alternative_forms"]
+			except:
+				print("Failed parsing response.")
+			self.waiting_for.pop(req_id)(results)
+
+		
+	def dictionary_request(self, lexemeId, on_load):
+		id = "d."+self.id+":"+lexemeId
+		self.waiting_for[id] = on_load
+		dictionary(lexemeId, id)
+
+
 	def __init__(self, search_results, native_language, addon_widget):
 		self.addon_widget = addon_widget
 		self.widget = qt.QTabWidget()
+
+		self.id = str(uuid4())
+		self.waiting_for = {}
+		nam.finished.connect(self.on_response)
+
+		qt.qconnect(self.widget.destroyed, lambda p: self.cleanup())
+
 		self.shown_funcs = []
 		for r in search_results:
 			t, func = self.tab(r, native_language)
@@ -110,14 +145,12 @@ class Word:
 			self.shown_funcs.append(func)
 			if r["exactMatch"]:
 				self.widget.setCurrentWidget(t)
+				self.shown_funcs[-1]()
 		qt.qconnect(self.widget.currentChanged, lambda i : self.current_changed(i))
 		if len(self.shown_funcs) > 0:
 			self.shown_funcs[0]()
 
 class Widget:
-	def update_wrapper(self):
-		self.update(True)
-
 	def do_custom_search(self):
 		self.is_custom_search = True
 		self.update(True)
@@ -128,6 +161,9 @@ class Widget:
 		self.note: Note
 		self.ed: Editor
 		self.is_custom_search = False
+
+		self.id = str(uuid4())
+		self.waiting_for = set()
 
 		splitter = qt.QSplitter(qt.Qt.Vertical)
 
@@ -153,7 +189,7 @@ class Widget:
 		refresh = qt.QPushButton()
 		refresh.setText("S&earch main field")
 		refresh.setAutoDefault(False)
-		qt.qconnect(refresh.clicked, self.update_wrapper)
+		qt.qconnect(refresh.clicked, lambda : self.update(True))
 
 		header1 = qt.QWidget()
 		header1_layout = qt.QHBoxLayout(header1)
@@ -223,6 +259,8 @@ class Widget:
 
 		qt.qconnect(self.group_box.destroyed, lambda p: self.cleanup())
 
+		nam.finished.connect(self.on_response)
+
 		self.focus_hook_lambda = lambda n, i: self.update_on_focus(n, i)
 		self.load_hook_lambda = lambda ed: self.update_on_load(ed)
 		self.notetype_hook_lambda = lambda nt: self.update_on_note_type_change(nt)
@@ -240,6 +278,25 @@ class Widget:
 		gui_hooks.editor_did_focus_field.remove(self.focus_hook_lambda)
 		gui_hooks.editor_did_load_note.remove(self.load_hook_lambda)
 		gui_hooks.current_note_type_did_change.remove(self.notetype_hook_lambda)
+		nam.finished.disconnect(self.on_response)
+
+	def on_response(self, r):
+		req_id = r.request().attribute(qt.QNetworkRequest.User)
+		w = r.request().attribute(qt.QNetworkRequest.UserMax)
+		if req_id in self.waiting_for:
+			self.waiting_for.remove(req_id)
+			results = list()
+			try:
+				results = json.loads(str(r.readAll(), "utf-8"))["results"]
+			except:
+				print("Failed parsing response.")
+			self.content.addTab(Word(results, self.nl_selector.currentText(), self).widget, w)
+
+		
+	def search_request(self, languageId, query, uiLanguageId):
+		id = "s."+self.id+":"+languageId+"."+uiLanguageId+"."+query
+		self.waiting_for.add(id)
+		search(languageId, query, uiLanguageId, id)
 
 	def update(self, request=False):
 		global config
@@ -270,21 +327,21 @@ class Widget:
 
 		if i == -1:
 			self.field_selector.setCurrentIndex(0)
-		
+
 		if request:
+			t = ""
 			if self.is_custom_search:
 				t = self.custom_search.text()
-				self.content.clear()
-				for w in get_words(ignore(t, self.ignored.text())):
-					self.content.addTab(Word(search(self.tl_selector.currentText(), w, self.nl_selector.currentText()), self.nl_selector.currentText(), self).widget, w)
 				self.is_custom_search = False
 			else:
-				for f, t in self.note.items():
+				for f, c in self.note.items():
 					if f == self.field_selector.currentText():
-						self.content.clear()
-						for w in get_words(ignore(t, self.ignored.text())):
-							self.content.addTab(Word(search(self.tl_selector.currentText(), w, self.nl_selector.currentText()), self.nl_selector.currentText(), self).widget, w)
-	
+						t = c
+						break
+			self.content.clear()
+			for w in get_words(ignore(t, self.ignored.text())):
+				self.search_request(self.tl_selector.currentText(), w, self.nl_selector.currentText())
+
 	def update_on_focus(self, n: Note, i: int = 0):
 		self.note = n
 		self.update()
@@ -315,21 +372,18 @@ def parse_deck_rule(s: str, deck: str = "*") -> str:
 		print("Couldn't parse deck rule: ", s, deck)
 	return ""
 
-def dictionary(lexemeId):
+def dictionary(lexemeId, req_id):
 	url = "https://www.duolingo.com/api/1/dictionary_page?lexeme_id=%s" % lexemeId
-	req = requests.request("GET", url)
-	try:
-		return req.json()["alternative_forms"]
-	except:
-		return list()
+	req = qt.QNetworkRequest(qt.QUrl(url))
+	req.setAttribute(qt.QNetworkRequest.User, req_id)
+	nam.get(req)
 
-def search(languageId, query, uiLanguageId):
+def search(languageId, query, uiLanguageId, req_id):
 	url = "https://duolingo-lexicon-prod.duolingo.com/api/1/search?exactness=1&languageId=%s&query=%s&uiLanguageId=%s" % (languageId, query, uiLanguageId)
-	req = requests.request("GET", url)
-	try:
-		return req.json()["results"]
-	except:
-		return list()
+	req = qt.QNetworkRequest(qt.QUrl(url))
+	req.setAttribute(qt.QNetworkRequest.User, req_id)
+	req.setAttribute(qt.QNetworkRequest.UserMax, query)
+	nam.get(req)
 
 def conv_html(s: str) -> str:
 	return s.replace("<span class=\"highlighted\">", "<b>").replace("</span>", "</b>")
@@ -360,7 +414,7 @@ def get_words(s: str) -> List[str]:
 
 def setup_button(buttons: List[str], ed: Editor) -> None:
 	widget = Widget(ed)
-	btn = ed.addButton(icon(), str(id(widget)), lambda ed: widget.button_clicked(), "Duolingo dictionary", toggleable=True, disables=False)
+	btn = ed.addButton(icon(), str(id(widget)), lambda ed, widget=widget: widget.button_clicked(), "Duolingo dictionary", toggleable=True, disables=False)
 	buttons.insert(-1, btn)
 
 def setup() -> None:
